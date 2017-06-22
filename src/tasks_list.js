67471a929e0bb9549c20972d5289fc3c248caedb
Younus Mahmood <younus@lionapps.com>
2017-06-22 14:52:28 -0500

    Added ability to add tasks to task list

--- src/tasks_list.js
+++ src/tasks_list.js
@@ -4,21 +4,25 @@ export default class TasksList extends Component {
     constructor(props){
         super(props);
         
-        this.state = { tasks: ['Build To Do List', 'Eat Brownies'] }
+        this.state = { tasks: [] }
+        this.handleClick = this.handleClick.bind(this);
     }
     
     renderTasksList() {
-        return (
-            this.state.tasks.map((tasks) => <li>{tasks}</li>)
-        )
+        return this.state.tasks.map((tasks) => <li>{tasks}</li>);                  
     }
+            
+    handleClick(){
+            this.setState({ tasks: this.state.tasks.concat(this.textInput.value)});
+            this.textInput.value = '';
+        }
         
     render() {
 
         return (
             <div>
-            <input />
-            <button >Enter</button>
+            <input ref={(thisInput) => {this.textInput = thisInput}} />
+            <button onClick={this.handleClick} >Enter</button>
                 <ul>
                    {this.renderTasksList()}
                 </ul>
