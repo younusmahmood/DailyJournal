aa991e046da597da6f9d8af912ff2b427e27c13c
Younus Mahmood <younus@lionapps.com>
2017-06-22 11:15:42 -0500

    Added state to hold tasks

--- src/tasks_list.js
+++ src/tasks_list.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 
 export default class TasksList extends Component {
+    constructor(props){
+        super(props);
+        
+        this.state = { tasks: ['Build To Do List', 'Eat Brownies'] }
+    }
     
+    renderTasksList() {
+        return (
+            this.state.tasks.map((tasks) => <li>{tasks}</li>)
+        )
+    }
+        
     render() {
+
         return (
             <div>
             <input />
             <button >Enter</button>
                 <ul>
-                    <li>{this.props.task}</li>
+                   {this.renderTasksList()}
                 </ul>
             </div>
         )
